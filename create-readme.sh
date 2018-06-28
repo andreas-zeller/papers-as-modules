372d8fba4835e961ed2fbb632f2510372e665a15
Andreas Zeller <zeller@cs.uni-saarland.de>
2018-06-28 09:42:24 +0200

    Better description

--- create-readme.sh
+++ create-readme.sh
@@ -3,5 +3,5 @@
 
 jupyter nbconvert --to markdown Papers-as-Modules.ipynb
 mv Papers-As-Modules.md README.md
-git commit -m "Generated automatically" README.md
+git commit -m "Generated automatically from Papers-as-Modules.ipynb" README.md
 exit 0
